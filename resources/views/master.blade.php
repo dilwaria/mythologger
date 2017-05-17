a4868ca26ea9fa2b2c9d17b3b425d537a24c9367
dilwaria <dilwaria@gmail.com>
2017-05-17 12:02:21 +0530

    ga alaytics

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -48,7 +48,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-99114732-1', 'auto');
+  ga('create', 'UA-99114732-1', 'mythologger.com');
   ga('send', 'pageview');
 
 </script>
