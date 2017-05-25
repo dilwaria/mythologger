0d63ac25421504ccaec8ffa1ba02ecda6df672cd
abhinav <abhinav.sharma@aspiringminds.in>
2017-05-25 22:55:22 +0530

    adding the check case for local setup......

--- app/Http/Middleware/Redirections.php
+++ app/Http/Middleware/Redirections.php
@@ -17,7 +17,7 @@ class Redirections
     {
         $url=$_SERVER['SERVER_NAME'];
         $uri= $_SERVER['REQUEST_URI'];
-        if($url!="www.mythologger.com"){
+        if($url!="www.mythologger.com" && !getenv('LOCAL_SETUP')){
             $fullUrl= "https://www.mythologger.com$uri";
             header("Location: $fullUrl",TRUE,301);
             exit();
