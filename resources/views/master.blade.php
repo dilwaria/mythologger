4d05cdc91a0dfdbed654a51bf1e75eaaaefb0d44
abhinav <abhinav.sharma@aspiringminds.in>
2017-05-29 16:51:59 +0530

    adding the transation btn at top.......

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,7 +9,11 @@
     <!-- Color Bars (above header)-->
 	<div class="color-bar-1"></div>
     <div class="color-bar-2 color-bg"></div>
-    
+    <div class="languageCheck">
+    	<div class="languageCheckText">
+    		Translate this article to <b>Italian</b>
+		</div>
+    </div>
     <div class="container">
 
      @include('header')
