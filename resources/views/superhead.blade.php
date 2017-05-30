d84e708f03101146a9be38d91271e7a505392984
dilwaria <dilwaria@gmail.com>
2017-05-30 21:42:22 +0530

    sitemap and adword

--- resources/views/superhead.blade.php
+++ resources/views/superhead.blade.php
@@ -71,5 +71,13 @@ $(document).ready(function () {
 
 </script>
 
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-3341786863476895",
+    enable_page_level_ads: true
+  });
+</script>
+
 </head>
 
