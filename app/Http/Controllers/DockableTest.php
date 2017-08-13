0e60df650fa6ac7e9be5eb31f4adfdee4eefb656
dilwaria <dilwaria@gmail.com>
2017-08-13 20:25:56 +0530

    dockable test

---
+++ app/Http/Controllers/DockableTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use DB;
+
+
+// 226451310967.224827743969 client id
+// 77beddce172677dbc206ab17573eb362  client secret key
+// igePHdSLSB9So4QwVAg3qaQI verifytoken
+class DockableTest extends Controller
+{
+	public function __construct(){
+        
+    }
+
+    public function todolist(){
+
+
+    	return "True";
+    }
+
+}
