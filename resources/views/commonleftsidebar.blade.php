8f2e06e53b7f731bf999ebb0d4b53e30d0b8ed3c
abhinav <abhinav.sharma@aspiringminds.in>
2017-08-28 13:00:17 +0530

    adding the add to the top.....

--- resources/views/commonleftsidebar.blade.php
+++ resources/views/commonleftsidebar.blade.php
@@ -1,15 +1,4 @@
-<div class="gAds">
-                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                <!-- descriptionPage -->
-                <ins class="adsbygoogle"
-                     style="display:block"
-                     data-ad-client="ca-pub-3341786863476895"
-                     data-ad-slot="2734358964"
-                     data-ad-format="auto"></ins>
-                <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
-            </div>
+
 
              <h5 class="title-bg f16">Do you know ?</h5>
             
